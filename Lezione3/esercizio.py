5d7a61b0bf1126a94d8695f3c70746a6486073d8
Dario <104058513+DarioIEL@users.noreply.github.com>
2025-07-21 18:45:26 +0200

    Add files via upload

--- Lezione3/esercizio.py
+++ Lezione3/esercizio.py
@@ -4,4 +4,12 @@ numeri = [6,7,22,34,6,8,9,11,27,87,75,4,42]
 # Dato il seguente array di voti calcola la media
 voti = [85, 66, 90, 100, 50, 92, 67]
 
+#Utilizzando l'array precedente stampa tutti i numeri, al primo multiplo di 25 esci dal ciclo (utilizza break)
+
+#Utilizzando lo stesso array stampa tutti i numeri, salta il numero 100 (utilizza continue)
+
 # Stampa i numeri da 1 a 100. Per tutti i numeri multipli di 3 stampa "ZOOM", per i tutti i multipli di 5 stampa "BOOM", per i multipli di entrambi stampa "ZOOM BOOM"
+
+# Data la lista [2, 7, 1, 9, 3], trova il numero più grande
+
+# Conta quante volte appare ogni vocale nella parola 'programmazione'
